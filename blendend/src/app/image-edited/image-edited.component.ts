fb00909532851f9341a47145dbacd0b40e3e6307
Joelius300 <bstlng321@gmail.com>
2020-04-26 20:04:02 +0200

    Add beerslider for before/after view

--- blendend/src/app/image-edited/image-edited.component.ts
+++ blendend/src/app/image-edited/image-edited.component.ts
@@ -1,18 +1,26 @@
-import { Component, OnInit, Input } from '@angular/core';
+import { Component, OnInit, AfterViewInit, Input, ViewChild, ElementRef } from '@angular/core';
 import { ImageEdited } from '../model/image-edited';
+import BeerSlider from 'beerslider';
 
 @Component({
   selector: 'app-image-edited',
   templateUrl: './image-edited.component.html',
   styleUrls: ['./image-edited.component.scss']
 })
-export class ImageEditedComponent implements OnInit {
+export class ImageEditedComponent implements OnInit, AfterViewInit {
   @Input()
   image: ImageEdited;
+
+  @ViewChild('slider')
+  slider: ElementRef;
 
   constructor() { }
 
   ngOnInit(): void {
   }
 
+  ngAfterViewInit(): void {
+    // tslint:disable-next-line: no-unused-expression
+    new BeerSlider(this.slider.nativeElement);
+  }
 }
