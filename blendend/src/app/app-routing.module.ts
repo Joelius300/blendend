3146829bebb799a905c4d8943336b6598877fda8
Joelius300 <bstlng321@gmail.com>
2020-05-07 22:20:04 +0200

    Add route-guard to /user
    Also remove unnecessary class on youtube-iframes

--- blendend/src/app/app-routing.module.ts
+++ blendend/src/app/app-routing.module.ts
@@ -2,10 +2,11 @@ import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { HomeComponent } from './home/home.component';
 import { UserComponent } from './user/user.component';
+import { UserGuard } from './user.guard';
 
 const routes: Routes = [
   { path: '', component: HomeComponent, pathMatch: 'full' },
-  { path: ':user', component: UserComponent },
+  { path: ':user', component: UserComponent, canActivate: [UserGuard] },
   { path: '**', redirectTo: '' },
 ];
 
---
+++ blendend/src/app/user.guard.ts
@@ -0,0 +1,23 @@
+import { Injectable } from '@angular/core';
+import { CanActivate, ActivatedRouteSnapshot, RouterStateSnapshot, UrlTree, Router } from '@angular/router';
+import { Observable } from 'rxjs';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class UserGuard implements CanActivate {
+  private validUsers = ['joel', 'noah', 'yanik'];
+
+  constructor(private router: Router) { }
+
+  canActivate(
+    next: ActivatedRouteSnapshot,
+    state: RouterStateSnapshot): Observable<boolean | UrlTree> | Promise<boolean | UrlTree> | boolean | UrlTree {
+    const valid = this.validUsers.includes(next.params.user);
+    if (!valid) {
+      this.router.navigate(['/']);
+    }
+
+    return valid;
+  }
+}
